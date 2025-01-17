b6e4e8cd798ec366f0ec544f9e1c23aae22c493f
Tobias Stein <tobiasstein1@gmx.net>
2025-01-17 20:17:46 +0100

    PID control working relatively good

--- src/main.cpp
+++ src/main.cpp
@@ -1,8 +1,8 @@
 #include <Arduino.h>
 #include <RP2040_PWM.h>
 #include <pico/stdlib.h>
+#include <hardware/gpio.h>
 #include "spid.h"
-
 
 #define Serial Serial1
 #define PIN_MOTOR_PWM 16
@@ -11,20 +11,20 @@
 #define PIN_SWITCH_LEFT 11
 #define PIN_LED_USER 17
 
-#define HALL_SENSOR_DETECTION_THRESHOLD 1000
-float pwm_frequency = 1000;
-RP2040_PWM PWM_instance(PIN_MOTOR_PWM, pwm_frequency, 0);
+// Motor speed measurement stuff
+#define PULSES_PER_REV 2
+#define RPM_AVERAGING_FILTER_SIZE 8
 
 #define K_P (0.1)
 #define K_I (0.1)
 #define K_D (0)
 #define PID_INTERVAL_S (0.5) // 500ms
 #define PID_MIN_OUT (0)
-#define PID_MAX_OUT (255)
-struct repeating_timer motorTimer, hallTimer;
+#define PID_MAX_OUT (100)
+struct repeating_timer motorTimer;
 spid_t pid;
 
-#define MAX_TARGET_RPM (600)
+#define MAX_TARGET_RPM (1600)
 volatile uint16_t targetRPM = 0;
 volatile uint16_t measuredRPM = 0;
 
@@ -41,33 +41,58 @@ bool motorLoop(struct repeating_timer* t){
     return true;
 }
 
-bool measurementLoop(struct repeating_timer* t){
-    static uint32_t lastRotationCompleteTime_us = 0;
-    static uint32_t lastRotationPeriod_us = 0;
-    uint32_t currentTime = time_us_32();
-    static bool risingEdge = false;
-    uint16_t hall = analogRead(PIN_HALL_SENSOR);
-    if(hall > HALL_SENSOR_DETECTION_THRESHOLD){
-        if(!risingEdge){
-            risingEdge = true;
-            lastRotationPeriod_us = currentTime - lastRotationCompleteTime_us;
-            lastRotationCompleteTime_us = currentTime;
-            measuredRPM = (1/((float)lastRotationPeriod_us / 1E6)) * 60 / 2;
-            digitalWrite(PIN_LED_USER, 0);
-        }
-    }
-    else{
+void gpio_callback(uint gpio, uint32_t events){
+    if(gpio == PIN_HALL_SENSOR && events & GPIO_IRQ_EDGE_RISE){
+        // signal detection using LED
         digitalWrite(PIN_LED_USER, 1);
-        risingEdge = false;
     }
-    // decay rpm if after a double the last rotation period
-    // no new completed rotation has been detected
-    // uint32_t tsinceFullRev_us = currentTime - lastRotationCompleteTime_us;
-    // if(tsinceFullRev_us > lastRotationPeriod_us){
-    //     measuredRPM = measuredRPM * 0.85;
-    // }
+    else if(gpio == PIN_HALL_SENSOR && events & GPIO_IRQ_EDGE_FALL){
+        static uint32_t lastPulseTime_us = 0;
+        static uint32_t pulseIntervals[PULSES_PER_REV] = {0};
+        const uint32_t pulseIntervalsArrSize = sizeof(pulseIntervals)/sizeof(pulseIntervals[0]);
+        // points to next index to write to
+        static uint32_t pulseIntervalsNextIdx = 0;
 
-    return true;
+        // signal detection using LED
+        digitalWrite(PIN_LED_USER, 0);
+
+        // initialize variable before measuring
+        if(lastPulseTime_us == 0){
+            lastPulseTime_us = time_us_32();
+            return;
+        }
+        // calculate time between the last 2 magnetic pulses
+        const uint32_t currentTime_us = time_us_32();
+        const uint32_t interval_us = currentTime_us - lastPulseTime_us;
+        lastPulseTime_us = currentTime_us;
+
+        // add it to array of pulse times for averaging
+        pulseIntervals[pulseIntervalsNextIdx] = interval_us;
+        pulseIntervalsNextIdx = (pulseIntervalsNextIdx+1) % pulseIntervalsArrSize;
+
+        // average pulse times
+        uint32_t averageInterval_us = 0;
+        for(uint32_t i = 0; i < pulseIntervalsArrSize; i++){
+            averageInterval_us += pulseIntervals[i];
+        }
+        averageInterval_us /= pulseIntervalsArrSize;
+        // Serial.print(">intv:");
+        // Serial.println(interval_us);
+        // calculate actual axle rotation speed
+        static uint32_t rpmMeasurements[RPM_AVERAGING_FILTER_SIZE];
+        static const uint32_t rpmMeasurementsSize = sizeof(rpmMeasurements)/sizeof(rpmMeasurements[0]);
+        static uint32_t rpmMeasurementsNextIdx = 0;
+        uint32_t currentRPMMeasurement = (1/((float)averageInterval_us / 1E6)) * 60 * PULSES_PER_REV;
+        rpmMeasurements[rpmMeasurementsNextIdx] = currentRPMMeasurement;
+        rpmMeasurementsNextIdx = (rpmMeasurementsNextIdx+1) % rpmMeasurementsNextIdx;
+
+        uint32_t averagedRPM = 0;
+        for(uint32_t i = 0; i < rpmMeasurementsSize; i++){
+            averagedRPM += rpmMeasurements[i];
+        }
+        averagedRPM /= rpmMeasurementsSize;
+        measuredRPM = averagedRPM;
+    }
 }
 
 void setup()
@@ -80,20 +105,20 @@ void setup()
 
     // Pin configuration
     pinMode(PIN_MOTOR_PWM, OUTPUT);
-    pinMode(PIN_HALL_SENSOR, INPUT);
     pinMode(PIN_POTENTIOMETER, INPUT);
     pinMode(PIN_SWITCH_LEFT, INPUT);
     pinMode(PIN_LED_USER, OUTPUT);
+    digitalWrite(PIN_LED_USER, 1);
+
+    gpio_init(PIN_HALL_SENSOR);
+    gpio_set_dir(PIN_HALL_SENSOR, GPIO_IN);
+    gpio_set_irq_enabled_with_callback(PIN_HALL_SENSOR, GPIO_IRQ_EDGE_FALL | GPIO_IRQ_EDGE_RISE, true, gpio_callback);
 
     // PID controller initialization
     spid_init(&pid, K_P, K_I, K_D, PID_MIN_OUT, PID_MAX_OUT, PID_INTERVAL_S);
     // add callback for pid using hardware timer
     if(!add_repeating_timer_ms(PID_INTERVAL_S * 1000, motorLoop, NULL, &motorTimer)){
         Serial.println("Failed to create motor loop timer");
-        while(1);
-    }
-    if(!add_repeating_timer_ms(10, measurementLoop, NULL, &hallTimer)){
-        Serial.println("Failed to create sensor feedback loop timer");
         while(1);
     }
 }
